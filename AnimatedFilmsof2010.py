4e1842fe3189eadfca8f2e01d844f175f375716a
cas3y <cjmcnulty@massart.edu>
2017-04-26 12:48:17 -0400

    WMD Program 3

---
+++ AnimatedFilmsof2010.py
@@ -0,0 +1,131 @@
+Movies = []
+
+# Ask the user for a name.
+new_movie = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie2 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie2)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie3 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie3)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie4 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie4)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie5 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie5)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie6 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie6)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie7 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie7)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie8 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie8)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie9 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie9)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie10 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie10)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie11 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie11)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie12 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie12)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie13 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie13)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie14 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie14)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie15 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie15)
+
+# Show that the name has been added to the list.
+print(Movies)
+
+new_movie16 = raw_input(" Please tell me Best to Worst: Animated Films of 2010: ")
+
+# Add the new name to our list.
+Movies.append(new_movie16)
+
+# Show that the name has been added to the list.
+print(Movies)
+print "roger that"
